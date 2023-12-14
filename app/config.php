35c638367047c99a80fe2080a38e7db810feea48
rudyrod08 <rudyrodjr@hotmail.com>
2023-12-14 12:52:29 -0700

    db

--- app/config.php
+++ app/config.php
@@ -3,7 +3,7 @@
 define("BD_HOST", "localhost");
 define("BD_USER", "root");
 define("BD_PASS", "");
-define("BD", "punto_de_venta");
+define("BD", "khaluas_shop");
 define("BD_ENCODE", "utf8");
 
 ?>
